99abdc3c901affd0e5b55e1b1cff41f0696da512
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-22 20:23:02 -0800

    form will re-render after post request

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -34,23 +34,81 @@ app.use(express.static('client'));
 
 
 app.get('/', function(req, res) {
-  res.render('/client/index.html');
+  res.send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Training Form</title>
+    </head>
+    <body>
+      <h1>Training Form</h1>
+      <form action="/converter" method="POST">
+        Date: <input type="date" name="date">
+        <br>
+        Trainee First Name: <input type ="text" name="traineeFirstName">
+        Trainee Last Name: <input type ="text" name="traineeLastName">
+        <br>
+        Trainer First Name: <input type ="text" name="trainerFirstName">
+        Trainer Last Name: <input type ="text" name="trainerLastName">
+        <br>
+        Training Bench: 
+        <br>
+        <select name="trainingBench">
+          <option>Pre-Analytical</option>
+          <option>Extraction</option>
+          <option>Library Preparation</option>
+          <option>Sequencing</option>
+        </select>
+        <br>
+        <input type="submit">
+      </form>
+    </body>
+  </html>`);
 })
 
 
-app.post('/', function(req, res) {
+app.post('/converter', function(req, res) {
   console.log('req.body in server----------------------------------------------', req.body);
-  // var csvFile = convertToCSV(req.body);
-  // console.log(csvFile);
-  res.send(req.csv);
   // res.send(csvFile);
   // res.csv = csvFile;
-  console.log('res-----------------------------', res);
   // res.render('/', {csv: csvFile});
   // res.redirect('/'); //doesnt like this
 
   //The response from the server should contain the CSV report along with the form so the user 
   // can keep submitting indefinitely, without having to go back to the "form page".
+
+  var results = req.csv;
+
+  // res.render('/client/index.html');
+  res.send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Training Form</title>
+    </head>
+    <body>
+      <h1>Training Form</h1>
+      <h5 class="results"></h5>
+      <form action="/converter" method="POST">
+        Date: <input type="date" name="date">
+        <br>
+        Trainee First Name: <input type ="text" name="traineeFirstName">
+        Trainee Last Name: <input type ="text" name="traineeLastName">
+        <br>
+        Trainer First Name: <input type ="text" name="trainerFirstName">
+        Trainer Last Name: <input type ="text" name="trainerLastName">
+        <br>
+        Training Bench: 
+        <br>
+        <select name="trainingBench">
+          <option>Pre-Analytical</option>
+          <option>Extraction</option>
+          <option>Library Preparation</option>
+          <option>Sequencing</option>
+        </select>
+        <br>
+        <input type="submit">
+      </form>
+    </body>
+  </html>`);
 })
 
 app.listen(3000);
