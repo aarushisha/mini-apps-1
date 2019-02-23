bcf71b27f088a2da08739490297cedac2579a530
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-22 16:33:01 -0800

    middleware mostly works

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -6,45 +6,51 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(Converter); //middleware to convert json to csv? 
+app.use(Converter); //middleware to convert json to csv? 
 app.use(express.static('client'));
 
 
-var convertToCSV = function(obj) {
-  var str = '';
-  for (var key in obj) {
-    str += key + ',';
-  }
+// var convertToCSV = function(obj) {
+//   var str = '';
+//   for (var key in obj) {
+//     str += key + ',';
+//   }
 
-  var headerStr = str.slice(0, str.length - 1);
+//   var headerStr = str.slice(0, str.length - 1);
 
-  var strV = '';
-  for (var key in obj) {
-    strV += obj[key] + ',';
-  }
+//   var strV = '';
+//   for (var key in obj) {
+//     strV += obj[key] + ',';
+//   }
 
-  var valueStr = strV.slice(0, strV.length - 1);
+//   var valueStr = strV.slice(0, strV.length - 1);
 
-  var csv = headerStr + '\n' + valueStr;
+//   var csv = headerStr + "\n" + valueStr;
 
-  return csv;
-}
+//   return csv;
+// }
 
 
 
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/client/index.html');
   res.render('/client/index.html');
 })
 
 
 app.post('/', function(req, res) {
   console.log('req.body in server----------------------------------------------', req.body);
-  var csvFile = convertToCSV(req.body);
-  console.log(csvFile);
+  // var csvFile = convertToCSV(req.body);
+  // console.log(csvFile);
+  res.send(req.csv);
+  // res.send(csvFile);
+  // res.csv = csvFile;
+  console.log('res-----------------------------', res);
+  // res.render('/', {csv: csvFile});
+  // res.redirect('/'); //doesnt like this
 
-  res.send(csvFile);
+  //The response from the server should contain the CSV report along with the form so the user 
+  // can keep submitting indefinitely, without having to go back to the "form page".
 })
 
 app.listen(3000);
