a82ce891f97414706f574930261b14459303beb2
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-25 12:41:16 -0800

    added template for other pages

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,13 +1,103 @@
+
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 1,
+    }
+  }
+  render() {
+    if (this.state.step === 1) {
+        return (
+          <div>
+            <h1>Click below to check out!</h1>
+             <button id="checkout">Check Out</button>
+          </div>
+        ); 
+    }
+    if (this.state.step === 2) {
+      return (
+        <div>
+          <h1>Click below to check out!</h1>
+           <button id="checkout">Check Out</button>
+        </div>
+      ); 
+  }
+  if (this.state.step === 3) {
+    return (
+      <div>
+        <h1>Click below to check out!</h1>
+         <button id="checkout">Check Out</button>
+      </div>
+    ); 
+}
+if (this.state.step === 4) {
+  return (
+    <div>
+      <h1>Click below to check out!</h1>
+       <button id="checkout">Check Out</button>
+    </div>
+  ); 
+}
+if (this.state.step === 5) {
+  return (
+    <div>
+      <h1>Click below to check out!</h1>
+       <button id="checkout">Check Out</button>
+    </div>
+  ); 
+}
+    }
+  }
+
+
+ReactDOM.render(<HomePage />,  document.getElementById('home'));
+
+
+class Form1 extends React.Component {
   render() {
     return (
       <div>
         <h1>Click below to check out!</h1>
-        <button id="checkout">Check Out</button>
+        <button id="next1">Next</button>
       </div>
 
     ); 
   }
 }
 
-ReactDOM.render(<HomePage />,  document.getElementById('home'));
+class Form2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Click below to check out!</h1>
+        <button id="next2">Next</button>
+      </div>
+
+    ); 
+  }
+}
+
+class Form3 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Click below to check out!</h1>
+        <button id="next3">Next</button>
+      </div>
+
+    ); 
+  }
+}
+
+class Confirmation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Click below to check out!</h1>
+        <button id="purchase">Purchase</button>
+      </div>
+
+    ); 
+  }
+}
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -8,6 +8,11 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
+app.get('/form1', function(req, res) {
+  console.log(req);
+
+});
+
 
 
 
