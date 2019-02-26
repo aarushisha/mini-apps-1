41bd6732e56b983a2653b949f497299d60defe08
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-25 20:10:38 -0800

    adding mysql

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mysql =  require('mysql');
 var path = require('path');
 var app = express();
 app.use(express.static('public'));
@@ -8,14 +9,18 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-app.get('/form1', function(req, res) {
-  console.log(req);
-
-});
-
-
-
-
-
 app.listen(8000);
 
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: ''
+});
+
+db.connect(function(error) {
+  if (error) {
+    console.log('Error connecting to mysql: ', error);
+  } else {
+    console.log('mysql is connected')
+  }
+});
