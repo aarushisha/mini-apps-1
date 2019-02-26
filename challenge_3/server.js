60222d44159e0a9e403a87f7d4a62a7ad6b04013
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-26 09:20:19 -0800

    created table to hold user information

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -24,3 +24,29 @@ db.connect(function(error) {
     console.log('mysql is connected')
   }
 });
+
+db.query('CREATE DATABASE IF NOT EXISTS checkout', function(err, result) {
+  if (err) {
+    console.log('error in creating database', err);
+  } else {
+    console.log('database created');
+  }
+});
+
+db.query('USE checkout', function(err, result) {
+  if (err) {
+    console.log('error in using database checkout', err)
+  } else {
+    console.log("using database checkout")
+  }
+});
+
+var tableQuery = 'CREATE TABLE IF NOT EXISTS checkout_users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name CHAR, email CHAR, password CHAR, address1 CHAR, address2 CHAR, city CHAR, state CHAR, zipCode CHAR, phone CHAR, cc CHAR, exp CHAR, cvv CHAR, billingZipCode CHAR)'
+
+db.query(tableQuery, function(err, result) {
+  if (err) {
+    console.log('error in creating table checkout_users', err)
+  } else {
+    console.log('created table checkout_users');
+  }
+});
