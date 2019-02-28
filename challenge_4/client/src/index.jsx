4a404ca971192ddcbcf84118bcbc09be392da0ce
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-27 20:22:36 -0800

    created board

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -4,10 +4,79 @@ import {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 
+
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      turn: 1,
+      player: 'X',
+    }
+  }
   render() {
     return (
-        <h1>Connect 4!</h1>
+        <div>
+          <div id="player">Current player: {this.state.player}</div>
+          <br></br>
+          <table id="connect4board">
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
     )
   }
 }
